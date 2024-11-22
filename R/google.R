4bf2b0cc7c7ee895cca8904fdccebc0a0dea0967
belisards <adrianobf@gmail.com>
2024-11-22 19:26:26 -0300

    update google function

--- R/google.R
+++ R/google.R
@@ -3,6 +3,7 @@
 #' Get the Google Open Buildings Region Code
 #' @param centroid A numeric vector representing the centroid coordinates in the format \code{c(lon, lat)}.
 #' @return A character string representing the S2 region code.
+#' 
 get_open_buildings_region_code <- function(centroid) {
   s2_cell <- s2::as_s2_cell(s2::s2_lnglat(centroid[1], centroid[2]))
   s2_id <- as.character(s2::s2_cell_parent(s2_cell, level = 4))
@@ -11,42 +12,71 @@ get_open_buildings_region_code <- function(centroid) {
 }
 
 #' Download Google Open Buildings Data
-download_google_open_buildings <- function(bbox, output_dir = "data/", delete_compressed = TRUE) {
+#'
+#' @description Downloads Google Open Buildings data for a specified bounding box (bbox).
+#' If the data file already exists in the specified output directory, the download is skipped.
+#'
+#' @param bbox Numeric vector of length 4 specifying the bounding box in the format c(min_lon, min_lat, max_lon, max_lat).
+#' @param output_dir Character string specifying the output directory where the data will be saved. Defaults to "data/".
+#' @param delete_compressed Logical, if TRUE, deletes the compressed file after uncompressing. Defaults to TRUE.
+#'
+#' @return Character string representing the filename of the downloaded (or existing) data file.
+#'
+#' @examples
+#' bbox <- c(36.80, -1.30, 36.90, -1.20)
+#' download_google_open_buildings(bbox)
+#'
+#' @export
+#' 
+download_google_open_buildings <- function(bbox, output_dir = "data", delete_compressed = TRUE) {
   if (!dir.exists(output_dir)) {
     dir.create(output_dir, recursive = TRUE)
   }
-
-  # Calculate the centroid from bbox
+  
+  # Get the centroid from bbox
   centroid <- c(mean(c(bbox[1], bbox[3])), mean(c(bbox[2], bbox[4])))
   region_code <- get_open_buildings_region_code(centroid)
-
-  download_and_uncompress_buildings_data(region_code, output_dir, delete_compressed)
-  all_data <- load_all_buildings_data(output_dir)
-
-  return(all_data)
-}
-
-#' Download and Uncompress Google Open Buildings Data
-download_and_uncompress_buildings_data <- function(region_code, output_dir, delete_compressed) {
-  url <- sprintf("https://storage.googleapis.com/open-buildings-data/v3/polygons_s2_level_4_gzip/%s_buildings.csv.gz", region_code)
-  compressed_file_path <- file.path(output_dir, sprintf("%s_buildings.csv.gz", region_code))
-  uncompressed_file_path <- file.path(output_dir, sprintf("%s_buildings.csv", region_code))
-
-  download.file(url, compressed_file_path, mode = "wb")
-  print("File downloaded. Uncompressing it...")
-  R.utils::gunzip(compressed_file_path, destname = uncompressed_file_path, overwrite = TRUE)
-
-  if (delete_compressed && file.exists(compressed_file_path)) {
-    unlink(compressed_file_path)
+  
+  # Construct the filename
+  file_name <- file.path(output_dir, sprintf("%s_buildings.csv", region_code))
+  compressed_file_name <- file.path(output_dir, sprintf("%s_buildings.csv.gz", region_code))
+  
+  # Skip download if the file already exists in CSV or compressed
+  # exist_condition <- 
+  if (!file.exists(file_name) || !file.exists(compressed_file_name)) {
+    url <- sprintf("https://storage.googleapis.com/open-buildings-data/v3/polygons_s2_level_4_gzip/%s_buildings.csv.gz", region_code)
+    
+    # Download file with error handling
+    tryCatch({
+      download.file(url, compressed_file_name, mode = "wb")
+      message("File downloaded.")
+      
+      # Stream decompression directly to the destination file
+      message("gunzip - Uncompressing the file...")
+      # R.utils::gunzip(compressed_file_name, destname = file_name, overwrite = TRUE)
+      system(sprintf("gunzip -c %s > %s", compressed_file_name, file_name))
+    }, error = function(e) {
+      stop(sprintf("Failed to download or uncompress the file: %s", e$message))
+    })
+    
+    if (delete_compressed && file.exists(compressed_file_name)) {
+      unlink(compressed_file_name)
+    }
+  } else {
+    message("File already exists. Skipping download.")
+    return(file_name)
   }
+  
+  return(file_name)
 }
 
-# Load all buildings data from CSV files
-load_all_buildings_data <- function(data_folder) {
-  file_list <- list.files(data_folder, pattern = "\\.csv$", full.names = TRUE)
-  if (length(file_list) == 0) {
-    stop("No CSV files found in the specified folder.")
-  }
-  all_data <- do.call(rbind, lapply(file_list, sf::read_sf))
-  return(all_data)
-}
+# 
+# # Load all buildings data from CSV files
+# load_all_buildings_data <- function(data_folder) {
+#   file_list <- list.files(data_folder, pattern = "\\.csv$", full.names = TRUE)
+#   if (length(file_list) == 0) {
+#     stop("No CSV files found in the specified folder.")
+#   }
+#   all_data <- do.call(rbind, lapply(file_list, sf::read_sf))
+#   return(all_data)
+# }
